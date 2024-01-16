e0ce8cdb655a1bb8867135ad7716ea651cfca486
AzureAD\RahulShokeen <Rahul.Shokeen@qdap.qld.gov.au>
2024-01-16 17:13:54 +1000

    QGGPT-287 cors bug fix

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      // edit: updated to new key. Was previously `allowedForwardedHosts`
+      allowedOrigins: [process.env.NEXTAUTH_ALLOWED_ORIGINS],
+    },
+  },
 };
 
 module.exports = nextConfig;
