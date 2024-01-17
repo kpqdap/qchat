03df26dfedb7ab0e630147d994fee92bfc6342f8
Keith Oak <keith.oak@chde.qld.gov.au>
2024-01-17 17:17:40 +1000

    modal changes

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -49,25 +49,22 @@ export default function Modal(props: ModalProps): ReturnType<FC> {
         <div className={`${"modal"} ${props.open ? "display-block" : "display-none"}`}>
             <div className="modal-main">
                 <div className="modal-head">
-                <Typography variant="h4" className="text-primary">
+                <Typography variant="h4" className="text-primary p-2">
                 Submit your feedback
                 </Typography>
                 </div>
 
-                <div className="col-span-2 gap-5 flex flex-col flex-1">
-                <textarea
-                    placeholder="Enter your feedback here"
-                    ref={textAreaRef}
-                    rows={6}
-                    cols={50}
-                    className="textarea-with-spacing"
-                    onChange={(event) =>  handleFeedbackChange()}
-                    // defaultValue={"Enter your feedback here..."}
-                />
-                </div>
-
-                <div className="reason-buttons">
-
+                  <div className="col-span-2 gap-5 flex flex-col flex-1">
+                    <textarea
+                        placeholder="Please provide any additional details about the message or youre feedback, our team will not reply directly but it will assist us in improving our service."
+                        ref={textAreaRef}
+                        rows={6}
+                        cols={50}
+                        className="textarea-with-spacing p-4"
+                        onChange={(event) =>  handleFeedbackChange()}
+                    />
+                  </div>
+                <div className="reason-buttons p-4">
             <Tabs
               defaultValue={""}
               onValueChange={(value) => handleReasonChange(value)}
@@ -75,21 +72,21 @@ export default function Modal(props: ModalProps): ReturnType<FC> {
               <TabsList className="flex w-full justify-center gap-5" >
                 <TabsTrigger
                   value="Unsafe"
-                  className="flex items-center justify-center flex-2"
+                  className="flex items-center justify-center flex-2 gap-2"
                   disabled={!areTabsEnabled}
                 >
                   <Ban size={20} /> Unsafe
                 </TabsTrigger>
                 <TabsTrigger
                   value="Inaccurate"
-                  className="flex items-center justify-center flex-2"
+                  className="flex items-center justify-center flex-2 gap-2"
                   disabled={!areTabsEnabled}
                 >
                   <XCircle size={20} /> Inaccurate
                 </TabsTrigger>
                 <TabsTrigger
                   value="Unhelpful"
-                  className="lex items-center justify-center flex-2"
+                  className="flex items-center justify-center flex-2 gap-2"
                   disabled={!areTabsEnabled}
                 >
                   <FileQuestion size={20} /> Unhelpful
@@ -98,8 +95,7 @@ export default function Modal(props: ModalProps): ReturnType<FC> {
             </Tabs>
 
           </div>
-
-                <div className="btn-container">
+                <div className="btn-container flex items-center justify-center flex-2 gap-2 p-4">
                     <button type="button" className="btn" onClick={handleSubmit}>Submit</button>
                     <button type="button" className="btn" onClick={props.onClose}>Close</button>
                 </div>
