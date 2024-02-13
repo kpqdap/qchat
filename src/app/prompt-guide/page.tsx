498b9ddf9ca15229da0408366124347ea729ace4
AzureAD\RahulShokeen <Rahul.Shokeen@qdap.qld.gov.au>
2024-02-13 17:02:07 +1000

    added to public folder

--- src/app/prompt-guide/page.tsx
+++ src/app/prompt-guide/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
 
 const loadContent = async () => {
   return await fs.readFile(
-    process.cwd() + "/prompt-guide/guide.md",
+    process.cwd() + "/guide.md",
     "utf8"
   );
 };
