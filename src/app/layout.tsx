c2bde52eb5ef36d79dc08ff87daef0dcfa8f26f1
Keith Oak <keith.oak@chde.qld.gov.au>
2024-02-20 17:16:06 +1000

    Update header and navbar styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
                 <Header />
                 <NavBar />
               </header>
-              <div className="bg-lime-500 h-1"></div>
+              <div className="bg-designAccent h-1"></div>
               <main className="main flex flex-col w-full h-75vh">
                 <div
                   className={cn(
