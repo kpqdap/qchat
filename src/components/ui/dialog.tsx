e439b0b4def391408aac887a4c769e575633b448
Keith Oak <keith.oak@chde.qld.gov.au>
2024-03-05 20:40:39 +1000

    Update profile dialog content

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -11,12 +11,12 @@ export const Dailog = () => (
     </Dialog.Trigger>
     <Dialog.Portal>
       <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
-      <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+      <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,&apos;_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px&apos;] focus:outline-none">
         <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
           Edit profile
         </Dialog.Title>
         <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
-          Make changes to your profile here. Click save when you're done.
+          Make changes to your profile here. Click save when you are done.
         </Dialog.Description>
         <fieldset className="mb-[15px] flex items-center gap-5">
           <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="name">
