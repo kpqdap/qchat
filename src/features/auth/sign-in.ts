359338475b13fba85af48193efb1892880a89f8c
Keith Oak <keith.oak@chde.qld.gov.au>
2024-03-18 11:58:14 +1000

    Fix createUserRecord function syntax

--- src/features/auth/sign-in.ts
+++ src/features/auth/sign-in.ts
@@ -79,7 +79,7 @@ function createTenantRecord(user: UserRecord, groupAdmins: string[]): TenantReco
   }
 }
 
-function createUserRecord(user: User | AdapterUser): UserRecord { {
+function createUserRecord(user: User | AdapterUser): UserRecord {
   const now = new Date()
   const userRecord: UserRecord = {
     id: hashValue(user.upn),
