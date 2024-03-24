ec29a3477d832f7c8579f61adefbf242d6ef475a
Keith Oak <keith.oak@chde.qld.gov.au>
2024-03-24 20:23:02 +1000

    Fix bug in login functionality

--- src/app-global.ts
+++ src/app-global.ts
@@ -1,4 +1,4 @@
 const APP_VERSION = "1.3.1"
 export default APP_VERSION
 export const signInProvider = "azure-ad"
-export const SUPPORT_EMAIL = "qchat@chde.qld.gov.au"
+export const SUPPORT_EMAIL = "qchatsupport@chde.qld.gov.au"
